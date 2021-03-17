bd04ab59d821b2ccb82bc201dcc100a61bff282b
André Linde <thedevpunk@gmail.com>
2021-03-17 22:21:21 +0100

    added jsencrypt

---
+++ src/blockchain/keys.ts
@@ -0,0 +1,31 @@
+
+const montyKeyPair = {
+
+  privateKey: `-----BEGIN RSA PRIVATE KEY-----
+  MIICXQIBAAKBgQDBbZvHw9vMr3rLnAwhzRs1IWMU9Ky/DA1SP/xnCg8dAilKGs8F
+  Yo1tZlGw5yMwMqfa78/XCjAilp+xjGpfec72O6BZIgcyMkM6/h4vawVyN7TDQheN
+  NZXT/rfaJ7vidZptLHDNYXf5UtUBS8f0wt8wfrq8rJKWJ0oUQvJsK0kRuQIDAQAB
+  AoGAba8k9iuHOVOMs2birkwrkH5FPD3es1s3NpvYGennVhDCryXynOAcyJuL2V/I
+  YW1oJ8tzNUKutv5owfqY21ksVRAL2ZRopbyY630KPHq5twx9M4uKHNeQZr4nQfNi
+  Y5xmt4VJBWilq3HRjlVBqpHV0diMdThTeS7RbFVzDwc8NzECQQD/RU3zDjzq0X49
+  wFl2f50zpazn7ZudCHniDEebcWOHh0zEHAQdYOuKf38ZQe3q9Jxz7Rn0ovAq9A3O
+  4jhySru1AkEAwfsTHgnv/KoQh9AyPdtwHFNd2bjNnn7NHOXEMkhW+IeArqVgsV6s
+  dt6otK0dGpTMDrpHw2acSp0UbJ0xrLkodQJAX8uQTxAeyDhJ2b6fVBrb0Y/w7TB7
+  31hY9P2Kuci5gxjBNC8XVDcU7WqfNclKEYwuUkq+HNIwss3XiLXYXPuyqQJBAICV
+  H5z/1VbUnFSKi2M5fF0qXcGeoJ35S5lmLOFv1kaz+3bJJQktyoCZI5dwUSR3kbQJ
+  gtxhviQvb4R3xp/JtKkCQQCMQg9itsxCWrayr17svsZKUNzpoyrTdShX7X+1koT4
+  GGNTDb3imU6cvYW2ZLgXJvwGkliDyFqGFn3SE5ggiw6V
+  -----END RSA PRIVATE KEY-----`,
+
+  publicKey: `-----BEGIN PUBLIC KEY-----
+  MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBbZvHw9vMr3rLnAwhzRs1IWMU
+  9Ky/DA1SP/xnCg8dAilKGs8FYo1tZlGw5yMwMqfa78/XCjAilp+xjGpfec72O6BZ
+  IgcyMkM6/h4vawVyN7TDQheNNZXT/rfaJ7vidZptLHDNYXf5UtUBS8f0wt8wfrq8
+  rJKWJ0oUQvJsK0kRuQIDAQAB
+  -----END PUBLIC KEY-----`
+
+}
+
+export {
+  montyKeyPair
+}
--- src/blockchain/wallet.ts
+++ src/blockchain/wallet.ts
@@ -1,24 +1,35 @@
-import * as crypto from 'crypto';
+// import * as crypto from 'crypto';
 import Transaction from './transaction';
 import Chain from './chain';
+import { montyKeyPair } from './keys';
+import { JSEncrypt } from 'jsencrypt'; 
 
 class Wallet {
    public publicKey: string;
    public privateKey: string;
 
    constructor() {
-      const keypair = crypto.generateKeyPairSync('rsa', {
-         modulusLength: 2048,
-         publicKeyEncoding: { type: 'spki', format: 'pem' },
-         privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-      });
+      // const keypair = crypto.generateKeyPairSync('rsa', {
+      //    modulusLength: 2048,
+      //    publicKeyEncoding: { type: 'spki', format: 'pem' },
+      //    privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+      // });
 
-      this.publicKey = keypair.publicKey;
-      this.privateKey = keypair.privateKey;
+      this.publicKey = montyKeyPair.publicKey;
+      this.privateKey = montyKeyPair.privateKey;
    }
 
    sendMoney(amount: number, payeePublicKey: string) {
       const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+
+      var sign = new JSEncrypt();
+      sign.setPrivateKey(this.privateKey);
+      var signature = sign.sign($('#input').val(), CryptoJS.SHA256, "sha256");
+
+      // Verify with the public key...
+      var verify = new JSEncrypt();
+      verify.setPublicKey($('#pubkey').val());
+      var verified = verify.verify($('#input').val(), signature, CryptoJS.SHA256);
 
       transaction.sign(this.publicKey, this.privateKey);
 
